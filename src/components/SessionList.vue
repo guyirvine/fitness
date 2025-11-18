6242b3b54bda8b160c405c2a1eef9991e530b578
Guy Irvine <guyirvine@gmail.com>
2025-11-19 09:10:58 +1300

    Fixes #37: replace icon with text

--- src/components/SessionList.vue
+++ src/components/SessionList.vue
@@ -129,13 +129,7 @@ async function onTouchEnd(e, id) {
         @click="props.router.push('/workout')"
         class="workout manageWorkouts"
       >
-        <svg class="settings-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <circle cx="12" cy="12" r="3"></circle>
-          <path d="M12 1v6m0 6v6"></path>
-          <path d="M4.22 4.22l4.24 4.24m5.08 5.08l4.24 4.24"></path>
-          <path d="M1 12h6m6 0h6"></path>
-          <path d="M4.22 19.78l4.24-4.24m5.08-5.08l4.24-4.24"></path>
-        </svg>
+        Edit
       </li>
     </ul>
     <div class="hg-list-group">
